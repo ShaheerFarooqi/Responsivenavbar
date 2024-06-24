098e3fc5c24b0ae5f43f269743fee1ec0e68d6c0
ShaheerFarooqi <shaheerfarooqi56@gmail.com>
2024-06-24 05:00:35 +0500

    navbar

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+// HAMBURGER JS
+$(document).ready(function () {
+    $(".hamburger_btn").click(function () {
+        $(".hamburger_btn , .site_links").toggleClass("open");
+    });
+});
+// HAMBURGER JS
